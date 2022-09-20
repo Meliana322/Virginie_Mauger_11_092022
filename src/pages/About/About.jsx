0801d65f64049581da0e960437be7896a2d318b1
Mauger <meliana.322@gmail.com>
2022-09-20 14:27:04 +0200

    Finalisation css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Banner from '../../components/banner/Banner'
 import banner from '../../assets/image2.jpg'
-import bannerAbout from '../../components/banner/banner.module.css'
 import Dropdown from '../../components/Dropdown/Dropdown'
 import dataAbout from './dataAbout.json'
 import styles from './about.module.css'
@@ -9,7 +7,10 @@ import styles from './about.module.css'
 export default function About() {
   return (
     <>
-      <Banner image={banner} className={bannerAbout.banner_about} />
+      {/* <Banner image={banner} className={bannerAbout.banner_about} /> */}
+      <div className={styles.containerImage}>
+        <img src={banner} className={styles.image} alt="" />
+      </div>
       <div className={styles.dropdown}>
         {dataAbout.map((e, index) => (
           <Dropdown key={index} title={e.title} children={e.description} />
